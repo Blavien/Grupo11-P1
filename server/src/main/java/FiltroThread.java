40d24772a0de41363edfa7460ac89367ec3f2567
[pvpkiller01] <[henrique2777.vasconcelos98@gmail.com]>
2023-03-19 19:32:44 +0000

    teste do filtro para o main

--- server/src/main/java/FiltroThread.java
+++ server/src/main/java/FiltroThread.java
@@ -1,52 +1,44 @@
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
 import java.io.File;
-import java.util.List;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
-public class FiltroThread extends Thread{
-    public FiltroThread (){
+import java.util.List;
+import java.util.Scanner;
+import java.lang.Runnable;
 
+public class FiltroThread implements Runnable {
+    private String messagefiltered;
+    private String message;
+    private String file;
+
+    public FiltroThread(String message, String file) {
+        this.message = message;
+        this.file = file;
     }
-    public static void filtragem() {
 
-
-
+    public String getFilteredMessage() {
+        return messagefiltered;
     }
-    public static ArrayList<String> lerArquivoDePalavras() {
-        ArrayList<String> palavras = new ArrayList<String>();
+
+    public void run() {
+        File filtro = new File(file);
         try {
-            Scanner scanner = new Scanner(new File("server/Server.log"));
-            while (scanner.hasNext()) {
-                String linha = scanner.nextLine();
-                String[] palavrasLinha = linha.split("\s+");
-                for (String palavra : palavrasLinha) {
-                    palavras.add(palavra);
+            Scanner reader = new Scanner(filtro);
+            reader.useDelimiter("[,\\s]+");
+            List<String> messages = new ArrayList<>(Arrays.asList(message.split("[,\\s]+")));
+            System.out.println("Palavra " + messages);
+            while (reader.hasNext()) {
+                String word = reader.next();
+                while (messages.contains(word)) {
+                    System.out.println("Palavra " + word);
+                    messages.remove(word);
+
                 }
             }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Arquivo não encontrado.");
+            messagefiltered = String.join(" ", messages);
+            System.out.println("Final filtered message: " + messagefiltered);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return palavras;
     }
-
-    static ArrayList<String> words = lerArquivoDePalavras();
-
-    public static String removePalavrasRepetidas(String message) {
-        String[] messageArray = message.split(" ");
-        List<String> messageList = new ArrayList<>(Arrays.asList(messageArray));
-        messageList.removeAll(words);
-        String novaMensagem = String.join(" ", messageList);
-
-        return novaMensagem;
-    }
-
-
-
-
-
-
-
-
 }
