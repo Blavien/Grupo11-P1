6c36a562bf5085ba1f6e39db4104d333a02d580f
[pvpkiller01] <[henrique2777.vasconcelos98@gmail.com]>
2023-03-27 23:54:14 +0100

    Final

--- client/src/main/java/ServerConfigReader.java
+++ client/src/main/java/ServerConfigReader.java
@@ -12,11 +12,11 @@ public interface ServerConfigReader {
     static Properties getProperties() throws IOException {
             Properties properties = new Properties();
             FileInputStream inputStream = new FileInputStream("C:\\Users\\RP\\IdeaProjects\\Grupo11-P1\\server\\Server.config");
-
+            try {
                 properties.load(inputStream);
-
+            } finally {
                 inputStream.close();
-
+            }
             return properties;
         }
 
